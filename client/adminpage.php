c34216c52b17b09382cca6e62d56fd4647d61132
jaum537 <joaobarbosa5337@gmail.com>
2023-11-26 23:15:36 -0300

    Botão Login

--- client/adminpage.php
+++ client/adminpage.php
@@ -5,12 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="stylesheet\stylesheets.css">
+    <link rel="stylesheet" href="../stylesheet/stylesheets.css">
 </head>
 <body>
-    <form class="formadmin" action="">
-        
+    <div>
+    <form class="container min-vh-100 d-flex flex-column justify-content-center align-items-center" action="">
+        <h1>Login</h1>
+        <div class="inputbox">
+            <input type="text" placeholder="Usuário">
+        </div>
+        <div class="inputbox">
+            <input type="text" placeholder="Senha">
+        </div>
+        <button type="button" class="btn btn-primary">Entrar</button>
     </form>
+    </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
