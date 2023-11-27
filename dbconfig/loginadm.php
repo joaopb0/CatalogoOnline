56b5a37c75509b5a969355005be255ca35d412a2
jaum537 <joaobarbosa5337@gmail.com>
2023-11-26 23:27:23 -0300

    Inicio de Desenvolvimento Back-End

--- dbconfig/loginadm.php
+++ dbconfig/loginadm.php
@@ -2,7 +2,7 @@
     define('HOST', 'localhost');
     define('USER', 'root');
     define('PASS', '');
-    define('BASE', '');
+    define('BASE', 'loginadmpass');
 
     $conn = new mySQLi(HOST,USER,PASS,BASE);
 ?>
