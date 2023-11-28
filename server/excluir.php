522439532c09ce16a853868c3951d46a68be59d5
jaum537 <joaobarbosa5337@gmail.com>
2023-11-28 15:32:41 -0300

    Excluir criado

--- server/excluir.php
+++ server/excluir.php
@@ -1,16 +1,14 @@
 <?php
 include '../dbconfig/bancoprodutos.php';
 
-// Verifica se o parâmetro 'id' foi passado na URL
 if (isset($_GET['id']) && is_numeric($_GET['id'])) {
     $id = $_GET['id'];
 
-    // Prepara e executa a consulta de exclusão
     $stmt = $conn->prepare("DELETE FROM produtos WHERE id = ?");
     $stmt->bind_param("i", $id);
 
     if ($stmt->execute()) {
-        // Exclusão bem-sucedida, redireciona para a página principal
+
         header("Location: ../server/viewadm.php");
         exit();
     } else {
